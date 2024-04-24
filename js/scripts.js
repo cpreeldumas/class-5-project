a88a8ebeb94bb87da7809d92b8f34784badb30e0
Camille PD <52207575+cpreeldumas@users.noreply.github.com>
2024-04-23 21:55:22 -0400

    initiate repo

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3ByZWVsZHVtYXMiLCJhIjoiY2x1bHZ0dGJ0MGw4bTJpbGxwdm5jZmQ4cyJ9.ZusuHjH6O73kH4F2RmenWA';
+
+var mapOptions = {
+    container: 'map-container', // container ID
+    style: 'mapbox://styles/mapbox/dark-v11', // dark basemap
+    center: [-73.96143, 40.73941], // starting position [lng, lat]
+    zoom: 10, // starting zoom
+    //hash: true
+}
+
+// instantiate the map
+const map = new mapboxgl.Map(mapOptions);
+
+// add a navitation control
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-right');
